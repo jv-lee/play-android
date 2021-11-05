a2c3328c9841b70f8380c44f118fbfdd153b8f4d
lijiawei11 <lijiawei11@baidu.com>
2021-11-05 14:06:16 +0800

    update code.

--- module/home/src/main/kotlin/com/lee/playandroid/home/viewmodel/HomeViewModel.kt
+++ module/home/src/main/kotlin/com/lee/playandroid/home/viewmodel/HomeViewModel.kt
@@ -23,46 +23,48 @@ class HomeViewModel : CoroutineViewModel() {
 
     private val repository = ApiRepository()
 
-    val contentListLive = UiStatePageLiveData(initPage = 0)
+    val contentListLive = UiStatePageLiveData()
 
     /**
      * 获取contentList列表
      */
     fun loadListData(@LoadStatus status: Int) {
         launchMain {
-            contentListLive.pageLaunch(status, { page: Int ->
-                val dataList = mutableListOf<HomeContent>()
+            launchIO {
+                contentListLive.pageLaunch(status, { page: Int ->
+                    val dataList = mutableListOf<HomeContent>()
 
-                //首页添加header数据
-                if (page == 0) {
-                    val banner = repository.api.getBannerDataAsync().data
-                    dataList.add(HomeContent(bannerList = banner))
+                    //首页添加header数据
+                    if (page == contentListLive.getInitPage()) {
+                        val banner = repository.api.getBannerDataAsync().data
+                        dataList.add(HomeContent(bannerList = banner))
 
-                    val category = CategoryHelper.getHomeCategory()
-                    dataList.add(HomeContent(categoryList = category))
-                }
-
-                //获取网络item数据
-                val textItemData = repository.api.getContentDataAsync(page).data.apply {
-                    data.forEach {
-                        dataList.add(HomeContent(content = it))
+                        val category = CategoryHelper.getHomeCategory()
+                        dataList.add(HomeContent(categoryList = category))
                     }
-                }
 
-                //构建分页ui数据
-                PageUiData(textItemData.curPage, textItemData.total, dataList).also { newData ->
-                    contentListLive.applyData(
-                        contentListLive.getValueData<PageUiData<HomeContent>>()?.data,
-                        newData.data
-                    )
-                }
-            }, {
-                //缓存数据
-                cacheManager.getCache<PageUiData<HomeContent>>(Constants.HOME_CONTENT_CACHE_KEY)
-            }, {
-                //存储缓存数据
-                cacheManager.putCache(Constants.HOME_CONTENT_CACHE_KEY, it)
-            })
+                    //获取网络item数据
+                    val textItemData = repository.api.getContentDataAsync(page).data.apply {
+                        data.forEach {
+                            dataList.add(HomeContent(content = it))
+                        }
+                    }
+
+                    //构建分页ui数据
+                    PageUiData(textItemData.curPage, textItemData.total, dataList).also { newData ->
+                        contentListLive.applyData(
+                            contentListLive.getValueData<PageUiData<HomeContent>>()?.data,
+                            newData.data
+                        )
+                    }
+                }, {
+                    //缓存数据
+                    cacheManager.getCache<PageUiData<HomeContent>>(Constants.HOME_CONTENT_CACHE_KEY)
+                }, {
+                    //存储缓存数据
+                    cacheManager.putCache(Constants.HOME_CONTENT_CACHE_KEY, it)
+                })
+            }
         }
     }
 
