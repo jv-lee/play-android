af9ff3ce181dfe0472762a8f3612c87ff8a96118
lijiawei11 <lijiawei11@baidu.com>
2021-11-10 17:09:01 +0800

    insert system base ui code.

---
+++ module/system/src/main/kotlin/com/lee/playandroid/system/ui/NavigationFragment.kt
@@ -0,0 +1,24 @@
+package com.lee.playandroid.system.ui
+
+import com.lee.library.base.BaseFragment
+import com.lee.library.extensions.binding
+import com.lee.playandroid.system.R
+import com.lee.playandroid.system.databinding.FragmentNavigationBinding
+
+/**
+ * @author jv.lee
+ * @data 2021/11/10
+ * @description
+ */
+class NavigationFragment : BaseFragment(R.layout.fragment_navigation) {
+
+    private val binding by binding(FragmentNavigationBinding::bind)
+
+    override fun bindView() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
---
+++ module/system/src/main/kotlin/com/lee/playandroid/system/ui/SystemContentFragment.kt
@@ -0,0 +1,24 @@
+package com.lee.playandroid.system.ui
+
+import com.lee.library.base.BaseFragment
+import com.lee.library.extensions.binding
+import com.lee.playandroid.system.R
+import com.lee.playandroid.system.databinding.FragmentSystemContentBinding
+
+/**
+ * @author jv.lee
+ * @data 2021/11/10
+ * @description
+ */
+class SystemContentFragment : BaseFragment(R.layout.fragment_system_content) {
+
+    private val binding by binding(FragmentSystemContentBinding::bind)
+
+    override fun bindView() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
