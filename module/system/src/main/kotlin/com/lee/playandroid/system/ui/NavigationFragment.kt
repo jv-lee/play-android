6ed1832b060800d7b1bdf3ef4fc7bcea7b5152f7
lijiawei11 <lijiawei11@baidu.com>
2021-11-12 19:34:24 +0800

    insert NavigationTab code.

--- module/system/src/main/kotlin/com/lee/playandroid/system/ui/NavigationFragment.kt
+++ module/system/src/main/kotlin/com/lee/playandroid/system/ui/NavigationFragment.kt
@@ -1,9 +1,19 @@
 package com.lee.playandroid.system.ui
 
+import android.annotation.SuppressLint
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lee.library.base.BaseFragment
 import com.lee.library.extensions.binding
+import com.lee.library.extensions.findParentFragment
+import com.lee.library.extensions.setMargin
+import com.lee.library.extensions.toast
+import com.lee.library.mvvm.ui.observeState
+import com.lee.playandroid.library.common.entity.NavigationItem
 import com.lee.playandroid.system.R
 import com.lee.playandroid.system.databinding.FragmentNavigationBinding
+import com.lee.playandroid.system.ui.adapter.NavigationTabAdapter
+import com.lee.playandroid.system.viewmodel.NavigationViewModel
 
 /**
  * @author jv.lee
@@ -13,13 +23,43 @@ import com.lee.playandroid.system.databinding.FragmentNavigationBinding
  */
 class NavigationFragment : BaseFragment(R.layout.fragment_navigation) {
 
+    private val viewModel by viewModels<NavigationViewModel>()
+
     private val binding by binding(FragmentNavigationBinding::bind)
 
+    private lateinit var mNavigationTabAdapter: NavigationTabAdapter
+
     override fun bindView() {
+        mNavigationTabAdapter = NavigationTabAdapter(requireContext(), arrayListOf())
 
+        findParentFragment<SystemFragment>()?.parentBindingAction {
+            binding.rvTab.setMargin(top = it.toolbar.getToolbarLayoutHeight())
+        }
+        binding.rvTab.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTab.adapter = mNavigationTabAdapter
+
+        mNavigationTabAdapter.setOnItemClickListener { _, _, position ->
+            mNavigationTabAdapter.selectItem(position)
+        }
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun bindData() {
-
+        viewModel.navigationLive.observeState<List<NavigationItem>>(this, success = {
+            mNavigationTabAdapter.updateData(it)
+            mNavigationTabAdapter.notifyDataSetChanged()
+        }, error = {
+            toast(it.message)
+        }, loading = {
+        })
     }
+
+    override fun lazyLoad() {
+        viewModel.requestNavigationData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
 }
---
+++ module/system/src/main/kotlin/com/lee/playandroid/system/viewmodel/NavigationViewModel.kt
@@ -0,0 +1,43 @@
+package com.lee.playandroid.system.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.lee.library.cache.CacheManager
+import com.lee.library.extensions.getCache
+import com.lee.library.extensions.putCache
+import com.lee.library.mvvm.ui.UiState
+import com.lee.library.mvvm.ui.stateCacheFlow
+import com.lee.library.mvvm.viewmodel.CoroutineViewModel
+import com.lee.playandroid.system.constants.Constants
+import com.lee.playandroid.system.model.repository.ApiRepository
+import kotlinx.coroutines.flow.collect
+
+/**
+ * @author jv.lee
+ * @data 2021/11/12
+ * @description
+ */
+class NavigationViewModel : CoroutineViewModel() {
+
+    private val cacheManager = CacheManager.getDefault()
+
+    private val repository = ApiRepository()
+
+    private val _navigationLive = MutableLiveData<UiState>()
+    val navigationLive: LiveData<UiState> = _navigationLive
+
+    fun requestNavigationData() {
+        launchIO {
+            stateCacheFlow({
+                repository.api.getNavigationDataAsync().data
+            }, {
+                cacheManager.getCache(Constants.CACHE_KEY_NAVIGATION_CONTENT)
+            }, {
+                cacheManager.putCache(Constants.CACHE_KEY_NAVIGATION_CONTENT, it)
+            }).collect {
+                _navigationLive.postValue(it)
+            }
+        }
+    }
+
+}
