fe509a9da9267a397c256b90ecfa60d1b091a411
lijiawei11 <lijiawei11@baidu.com>
2021-11-23 19:52:29 +0800

    update details model class name.

--- module/details/src/main/java/com/lee/playandroid/details/DetailsFragment.kt
+++ module/details/src/main/java/com/lee/playandroid/details/DetailsFragment.kt
@@ -6,7 +6,7 @@ import com.lee.library.base.BaseFragment
 import com.lee.library.extensions.arguments
 import com.lee.library.extensions.binding
 import com.lee.library.extensions.setWebBackEvent
-import com.lee.playandroid.details.databinding.FragmentContentDetailsBinding
+import com.lee.playandroid.details.databinding.FragmentDetailsBinding
 import com.lee.playandroid.library.common.constant.KeyConstants
 import com.lee.playandroid.library.common.extensions.bindLifecycle
 
@@ -15,13 +15,13 @@ import com.lee.playandroid.library.common.extensions.bindLifecycle
  * @date 2020/3/24
  * @description 内容详情页
  */
-class ContentDetailsFragment :
-    BaseFragment(R.layout.fragment_content_details) {
+class DetailsFragment :
+    BaseFragment(R.layout.fragment_details) {
 
     private val title by arguments<String>(KeyConstants.ARG_PARAMS_TITLE)
     private val detailsUrl by arguments<String>(KeyConstants.ARG_PARAMS_URL)
 
-    private val binding by binding(FragmentContentDetailsBinding::bind)
+    private val binding by binding(FragmentDetailsBinding::bind)
 
     private lateinit var web: AgentWeb
 
