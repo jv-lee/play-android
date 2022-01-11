5924bd9be5d93d94eacc68d544ac61e57c3e986c
lijiawei11 <lijiawei11@baidu.com>
2022-01-11 15:34:34 +0800

    update FailedUiExtensions.kt ,out repeat message.

--- library/common/src/main/kotlin/com/lee/playandroid/library/common/extensions/FailedUiExtensions.kt
+++ library/common/src/main/kotlin/com/lee/playandroid/library/common/extensions/FailedUiExtensions.kt
@@ -1,5 +1,6 @@
 package com.lee.playandroid.library.common.extensions
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.lee.library.extensions.toast
@@ -13,10 +14,32 @@ import com.lee.playandroid.library.common.R
  * @description
  */
 
+private const val EXTENSIONS_KEY_THROWABLE_CODE = "extensionsKey:throwableCode"
+
+var FragmentActivity.saveThrowableCode: Int
+    get() = intent?.getIntExtra(EXTENSIONS_KEY_THROWABLE_CODE, 0) ?: 0
+    set(value) {
+        intent.putExtra(EXTENSIONS_KEY_THROWABLE_CODE, value)
+    }
+
+var Fragment.saveThrowableCode: Int
+    get() = arguments?.getInt(EXTENSIONS_KEY_THROWABLE_CODE, 0) ?: 0
+    set(value) {
+        arguments?.putInt(EXTENSIONS_KEY_THROWABLE_CODE, value) ?: kotlin.run {
+            arguments = Bundle().apply {
+                putInt(EXTENSIONS_KEY_THROWABLE_CODE, value)
+            }
+        }
+    }
+
 fun FragmentActivity.actionFailed(throwable: Throwable) {
-    if(isDestroyed) return
+    if (isDestroyed) return
     if (NetworkUtil.isConnected(applicationContext)) {
-        toast(HttpManager.getInstance().getServerMessage(throwable, "errorMsg"))
+        val throwableCode = throwable.hashCode()
+        if (throwableCode != saveThrowableCode) {
+            saveThrowableCode = throwableCode
+            toast(HttpManager.getInstance().getServerMessage(throwable, "errorMsg"))
+        }
     } else {
         toast(getString(R.string.network_not_access))
     }
@@ -25,7 +48,11 @@ fun FragmentActivity.actionFailed(throwable: Throwable) {
 fun Fragment.actionFailed(throwable: Throwable) {
     if (!isAdded) return
     if (NetworkUtil.isConnected(requireContext().applicationContext)) {
-        toast(HttpManager.getInstance().getServerMessage(throwable, "errorMsg"))
+        val throwableCode = throwable.hashCode()
+        if (throwableCode != saveThrowableCode) {
+            saveThrowableCode = throwableCode
+            toast(HttpManager.getInstance().getServerMessage(throwable, "errorMsg"))
+        }
     } else {
         toast(getString(R.string.network_not_access))
     }
