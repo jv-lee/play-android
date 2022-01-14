f99b07e7638565d40b739519367a4fad535c7cd7
lijiawei11 <lijiawei11@baidu.com>
2022-01-14 11:30:50 +0800

    update ViewModelExtensions.kt

--- library/base/src/main/java/com/lee/library/extensions/ViewModelExtensions.kt
+++ library/base/src/main/java/com/lee/library/extensions/ViewModelExtensions.kt
@@ -35,7 +35,8 @@ inline fun <reified VM : ViewModel> ComponentActivity.viewModelByFactory(
     return viewModels {
         createViewModelFactory(this, intent.extras) {
             val constructor = findMatchingConstructor<VM>(arrayOf(SavedStateHandle::class.java))
-            constructor!!.newInstance(it)
+            checkNotNull(constructor)
+            constructor.newInstance(it)
         }
     }
 }
@@ -46,9 +47,9 @@ inline fun <reified VM : ViewModel> ComponentActivity.viewModelByFactory(
 inline fun <reified VM : ViewModel> Fragment.viewModelByFactory(): Lazy<VM> {
     return viewModels {
         createViewModelFactory(this, arguments) {
-            val constructor =
-                findMatchingConstructor<VM>(arrayOf(SavedStateHandle::class.java))
-            constructor!!.newInstance(it)
+            val constructor = findMatchingConstructor<VM>(arrayOf(SavedStateHandle::class.java))
+            checkNotNull(constructor)
+            constructor.newInstance(it)
         }
     }
 }
