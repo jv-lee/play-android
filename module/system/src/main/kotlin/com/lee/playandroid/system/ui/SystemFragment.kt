41db8b62b8612622f67913334574428765b38f0c
lijiawei11 <lijiawei11@baidu.com>
2022-02-08 18:24:13 +0800

    remove SystemViewModel.kt

--- module/system/src/main/kotlin/com/lee/playandroid/system/ui/SystemFragment.kt
+++ module/system/src/main/kotlin/com/lee/playandroid/system/ui/SystemFragment.kt
@@ -1,7 +1,6 @@
 package com.lee.playandroid.system.ui
 
 import android.annotation.SuppressLint
-import androidx.fragment.app.viewModels
 import com.lee.library.adapter.core.UiPagerAdapter2
 import com.lee.library.base.BaseFragment
 import com.lee.library.extensions.bindRadioGroup
@@ -9,7 +8,6 @@ import com.lee.library.extensions.binding
 import com.lee.playandroid.library.common.ui.extensions.setThemeGradientBackground
 import com.lee.playandroid.system.R
 import com.lee.playandroid.system.databinding.FragmentSystemBinding
-import com.lee.playandroid.system.viewmodel.SystemViewModel
 
 /**
  * @author jv.lee
@@ -17,8 +15,6 @@ import com.lee.playandroid.system.viewmodel.SystemViewModel
  * @description 首页第三个Tab 体系页面
  */
 class SystemFragment : BaseFragment(R.layout.fragment_system) {
-
-    private val viewModel by viewModels<SystemViewModel>()
 
     private val binding by binding(FragmentSystemBinding::bind)
 
@@ -33,16 +29,9 @@ class SystemFragment : BaseFragment(R.layout.fragment_system) {
 
     @SuppressLint("NotifyDataSetChanged")
     override fun bindData() {
-        viewModel.fragmentsLive.observe(this, {
-            if (binding.vpContainer.adapter == null) {
-                mAdapter = UiPagerAdapter2(childFragmentManager, viewLifecycleOwner.lifecycle)
-                mAdapter?.addAll(it)
-                binding.vpContainer.adapter = mAdapter
-            } else {
-                mAdapter?.addAll(it)
-                mAdapter?.notifyDataSetChanged()
-            }
-        })
+        mAdapter = UiPagerAdapter2(childFragmentManager, viewLifecycleOwner.lifecycle)
+        mAdapter?.addAll(mutableListOf(SystemContentFragment(), NavigationFragment()))
+        binding.vpContainer.adapter = mAdapter
     }
 
     override fun onDestroyView() {
