91df083ab262a674a56ececd342f9199ed4fc837
lijiawei11 <lijiawei11@baidu.com>
2022-02-15 11:15:54 +0800

    PageUiState -> UiStatePage name.

--- module/search/src/main/kotlin/com/lee/playandroid/search/viewmodel/SearchResultViewModel.kt
+++ module/search/src/main/kotlin/com/lee/playandroid/search/viewmodel/SearchResultViewModel.kt
@@ -2,7 +2,7 @@ package com.lee.playandroid.search.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import com.lee.library.mvvm.livedata.LoadStatus
-import com.lee.library.mvvm.ui.PageUiState
+import com.lee.library.mvvm.ui.UiStatePage
 import com.lee.library.mvvm.ui.applyData
 import com.lee.library.mvvm.ui.pageLaunch
 import com.lee.library.mvvm.viewmodel.CoroutineViewModel
@@ -24,8 +24,8 @@ class SearchResultViewModel(handle: SavedStateHandle) : CoroutineViewModel() {
 
     private val repository = ApiRepository()
 
-    private val _searchResultFlow = MutableStateFlow<PageUiState>(PageUiState.Loading(0))
-    val searchResultFlow: StateFlow<PageUiState> = _searchResultFlow.asStateFlow()
+    private val _searchResultFlow = MutableStateFlow<UiStatePage>(UiStatePage.Loading(0))
+    val searchResultFlow: StateFlow<UiStatePage> = _searchResultFlow.asStateFlow()
 
     fun requestSearch(@LoadStatus status: Int) {
         launchIO {
