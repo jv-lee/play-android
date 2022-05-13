0aceb5291b3b790a06f837911c7eb90de6b966e2
lijiawei11 <lijiawei11@baidu.com>
2022-05-13 14:09:58 +0800

    update InjectBus.kt

--- library/base/src/main/java/com/lee/library/livedatabus/InjectBus.kt
+++ library/base/src/main/java/com/lee/library/livedatabus/InjectBus.kt
@@ -8,7 +8,7 @@ package com.lee.library.livedatabus
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class InjectBus(
-    val value: String,
-    val isActive: Boolean = true,
-    val isViscosity: Boolean = false
+    val value: String, // 事件key
+    val isActive: Boolean = true, // 当前为页面激活状态才接收消息
+    val isViscosity: Boolean = false // 当前是否支持延时粘性消息（暂时只支持最后一条消息接收，不支持队列）
 )
