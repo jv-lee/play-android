83ec9cd0494e355325abbecc4a704ccc3caaa519
lijiawei11 <lijiawei11@baidu.com>
2022-06-08 16:48:44 +0800

    update BaseDialogFragment.kt import code.

--- library/base/src/main/java/com/lee/playandroid/base/base/BaseDialogFragment.kt
+++ library/base/src/main/java/com/lee/playandroid/base/base/BaseDialogFragment.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.lee.playandroid.base.dialog.core.setBackDismiss
-import com.lee.playandroid.base.dialog.core.setFullWindow
+import com.lee.playandroid.base.dialog.extensions.setBackDismiss
+import com.lee.playandroid.base.dialog.extensions.setFullWindow
 
 /**
  * 通用DialogFragment基类
