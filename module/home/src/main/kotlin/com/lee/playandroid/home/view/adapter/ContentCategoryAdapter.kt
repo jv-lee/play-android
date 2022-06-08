aaf0d19c453b15d96db01c942375d93f5cd79581
lijiawei11 <lijiawei11@baidu.com>
2022-06-08 17:58:49 +0800

    update category item width code.

--- module/home/src/main/kotlin/com/lee/playandroid/home/view/adapter/ContentCategoryAdapter.kt
+++ module/home/src/main/kotlin/com/lee/playandroid/home/view/adapter/ContentCategoryAdapter.kt
@@ -8,6 +8,7 @@ import com.lee.playandroid.base.adapter.binding.ViewBindingAdapter
 import com.lee.playandroid.base.adapter.binding.ViewBindingHolder
 import com.lee.playandroid.base.adapter.item.ViewBindingItem
 import com.lee.playandroid.base.extensions.setImageTintCompat
+import com.lee.playandroid.home.R
 import com.lee.playandroid.home.databinding.ItemContentCategoryChildBinding
 import com.lee.playandroid.home.model.entity.HomeCategory
 
@@ -39,6 +40,15 @@ class ContentCategoryAdapter(context: Context, data: List<HomeCategory>) :
 
         override fun convert(holder: ViewBindingHolder, entity: HomeCategory, position: Int) {
             holder.getViewBinding<ItemContentCategoryChildBinding>().apply {
+                // 重新设置item宽度,宽度占屏幕一半并且减去margin值
+                val resource = root.context.resources
+                val layoutParams = root.layoutParams
+                val marginOffset = resource.getDimension(R.dimen.medium_offset)
+                layoutParams.width =
+                    ((resource.displayMetrics.widthPixels / 2) - (marginOffset * 2)).toInt()
+                root.layoutParams = layoutParams
+
+                // 设置内容信息
                 ivCategoryIcon.setImageTintCompat(entity.iconResId)
                 tvCategoryName.text = entity.name
             }
