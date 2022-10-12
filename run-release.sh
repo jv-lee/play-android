070469e3ae2898b2da0ac1c2820bea5f60237bb0
lijiawei11 <lijiawei11@baidu.com>
2022-10-12 14:44:07 +0800

    update run-release.sh code.

--- run-release.sh
+++ run-release.sh
@@ -1,5 +1,5 @@
 ./gradlew assembleRelease
 
-adb shell am force-stop com.lee.playandorid
+adb shell am force-stop com.lee.playandroid
 adb install -r ./app/build/outputs/apk/release/app-release.apk
-adb shell am start com.lee.playandorid/.MainActivity
+adb shell am start com.lee.playandroid/.MainActivity
