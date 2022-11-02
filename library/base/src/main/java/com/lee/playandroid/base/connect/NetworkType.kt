517869f8d967528eb43372dfa15fb5d945ec8096
lijiawei11 <lijiawei11@baidu.com>
2022-11-02 11:21:18 +0800

    add NetworkConnectManager.kt code.

---
+++ library/base/src/main/java/com/lee/playandroid/base/connect/NetworkType.kt
@@ -0,0 +1,12 @@
+package com.lee.playandroid.base.connect
+
+/**
+ * 网络类型密封类
+ * @author jv.lee
+ * @date 2022/11/2
+ */
+sealed class NetworkType {
+    object Wifi : NetworkType()
+    object Network : NetworkType()
+    object Other : NetworkType()
+}
